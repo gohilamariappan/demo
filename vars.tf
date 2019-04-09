05303969f3c2cafdc540dddec5aea732ba664a87
gohilamariappan <41056032+gohilamariappan@users.noreply.github.com>
2019-04-09 17:08:58 +0530

    changed

--- vars.tf
+++ vars.tf
@@ -5,5 +5,16 @@ default = ""
 
 variable "aws_secret_access_key"
 {
+
 default = ""
 }
+variable "ami_id" {
+type = "list"
+  default = [ "ami-007d5db58754fa284","ami-0a574895390037a62" ]  
+
+}
+
+variable "env"
+{
+   default = ""
+  }
