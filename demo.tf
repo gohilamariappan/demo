0431a3a63c5fe8bfb64e3168f726c91e895b9f7b
gohilamariappan <41056032+gohilamariappan@users.noreply.github.com>
2019-04-09 17:05:13 +0530

    changed

--- demo.tf
+++ demo.tf
@@ -1,18 +1,29 @@
 provider "aws" {
   access_key = "${var.aws_access_key_id}"
   secret_key = "${var.aws_secret_access_key}"
-   region     = "us-east-2"
+  region     = "ap-south-1"
 }
-resource "aws_instance" "web" {
 
-  ami = "ami-0c55b159cbfafe1f0"
-
+resource "aws_instance" "ubuntu18" {
+count="${var.env == "ubuntu18" ? 1:0}"
+  ami           = "${var.ami_id [0]}"
   instance_type = "t2.micro"
 
   tags {
 
-    Name = "HelloWorld"
+    Name = "Ec2 instance1"
 
   }
+}
+resource "aws_instance" "ubuntu16" {
+  count="${var.env == "ubuntu16" ? 1:0}"
 
+  ami           = "${var.ami_id [1]}"
+  instance_type = "t2.micro"
+
+  tags {
+
+    Name = "Ec2 instance2"
+
+  }
 }
